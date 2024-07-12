b6f1dacb34e2f042f6f359ac84a894e5718ea7fd
ChzuoL <chenzuoliu@gmail.com>
2024-07-12 09:27:13 -0700

    styling corners

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -92,6 +92,56 @@ Here's another paragraph using a different ID and a different look and
           Padded all around
         </div>
       </div>
+      <div id="wd-css-margins">
+        <h2>Margins</h2>
+        <div
+          className="wd-margin-bottom 
+            wd-padded-top-left 
+            wd-border-fat wd-border-red 
+            wd-border-solid 
+            wd-bg-color-yellow">
+          Margin bottom
+        </div>
+        <div
+          className="wd-margin-right-left 
+            wd-padded-bottom-right 
+            wd-border-fat wd-border-blue 
+            wd-border-solid 
+            wd-bg-color-yellow">
+          Margin left right
+        </div>
+        <div
+          className="wd-margin-all-around 
+            wd-padding-fat wd-border-fat 
+            wd-border-yellow 
+            wd-border-solid 
+            wd-bg-color-blue 
+            wd-fg-color-white">
+          Margin all around
+        </div>
+      </div>
+      <div id="wd-css-borders">
+        <h3>Rounded corners</h3>
+        <p className="wd-rounded-corners-top wd-border-thin 
+          wd-border-blue wd-border-solid wd-padding-fat">
+          Rounded corners on the top
+        </p>
+        <p className="wd-rounded-corners-bottom 
+          wd-border-thin wd-border-blue wd-border-solid 
+          wd-padding-fat">
+          Rounded corners at the bottom
+        </p>
+        <p className="wd-rounded-corners-all-around 
+          wd-border-thin wd-border-blue wd-border-solid 
+          wd-padding-fat">
+          Rounded corners all around
+        </p>
+        <p className="wd-rounded-corners-inline 
+          wd-border-thin wd-border-blue wd-border-solid 
+          wd-padding-fat">
+          Different rounded corners
+        </p>
+      </div>
     </div>
   );
 }

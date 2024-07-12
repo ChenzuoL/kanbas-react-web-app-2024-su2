fac4e37c527650f96a2e196176935574430abee1
ChzuoL <chenzuoliu@gmail.com>
2024-07-12 09:21:16 -0700

    padding

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -67,6 +67,31 @@ Here's another paragraph using a different ID and a different look and
           </span>
         </p>
       </div>
+      <div id="wd-css-borders">
+        <h2>Borders</h2>
+        <p className="wd-border-fat wd-border-red wd-border-solid">
+          Solid fat red border</p>
+        <p className="wd-border-thin wd-border-blue wd-border-dashed">
+          Dashed thin blue border</p>
+      </div>
+      <div id="wd-css-paddings">
+      <h2>Padding</h2>
+        <div className="wd-padded-top-left wd-border-fat 
+            wd-border-red wd-border-solid 
+            wd-bg-color-yellow">
+          Padded top left
+        </div>
+        <div className="wd-padded-bottom-right wd-border-fat 
+        wd-border-blue wd-border-solid 
+        wd-bg-color-yellow">
+          Padded bottom right
+        </div>
+        <div className="wd-padding-fat wd-border-fat 
+            wd-border-yellow wd-border-solid 
+            wd-bg-color-blue wd-fg-color-white">
+          Padded all around
+        </div>
+      </div>
     </div>
   );
 }
